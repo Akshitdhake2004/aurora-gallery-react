e427d854aff712587fb1e61ba1edb8cc2a9df58d
Akshitdhake2004 <138993305+Akshitdhake2004@users.noreply.github.com>
2025-05-02 00:36:02 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd /home/ec2-user/app
+
+# Source the NVM script
 source /home/ec2-user/.nvm/nvm.sh
-nvm use 22
+
+# Use Node.js version 18 for building
+nvm use 18
+
+# Install dependencies and build
 npm install
 npm run build
