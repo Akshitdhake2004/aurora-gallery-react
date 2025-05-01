8397ac913ec7aa4e6194f1e796b14165afdb8e8e
Akshitdhake2004 <138993305+Akshitdhake2004@users.noreply.github.com>
2025-05-02 00:36:17 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cd /home/ec2-user/app
+
+# Source the NVM script
 source /home/ec2-user/.nvm/nvm.sh
+
+# Use Node.js version 22 for runtime
 nvm use 22
+
+# Delete all pm2 processes, ignore errors if no processes exist
 pm2 delete all || true
+
+# Serve the application using pm2
 pm2 serve dist 8080 --spa --name aurora-gallery
